ac88b673c9d138d70c47e9cbed824e5df74b7137
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2020-12-20 13:36:11 -0500

    Added command line args to xyz.py.

--- xyz.py
+++ xyz.py
@@ -1,20 +1,33 @@
-# open the source file, read every line and split the lines
-filename = "source.xyz"
-# define the bounding box
-xmin = "2683475.00"
-ymin = "1242525.00"
-xmax = "2683540.00"
-ymax = "1242595.00"
+#!/usr/bin/env python3
 
-#open the source file, read every line of the are in the bounding box and append the coordinates
-
-col_num = 3
-col_data = []
-delimiter = ' '
-
-with open(filename) as f:
-                col_data.append(f.readline().split(delimiter)[col_num])
-
-print col_data
+import argparse
+import sys
 
 
+parser = argparse.ArgumentParser(
+  description="""
+Extracts regions bounded by the given arguments.  By default, reads
+from stdin and writes to stdout, with infinite upper and lower bounds.
+""")
+parser.add_argument('-infile', type=open, default=sys.stdin)
+parser.add_argument('-outfile', type=argparse.FileType('w'), default=sys.stdout)
+parser.add_argument('-xmin', type=int, default=-float('inf'))
+parser.add_argument('-ymin', type=int, default=-float('inf'))
+parser.add_argument('-xmax', type=int, default=float('inf'))
+parser.add_argument('-ymax', type=int, default=float('inf'))
+args = parser.parse_args()
+
+# Open the source file, read every line of the are in the bounding box
+# and append the coordinates.
+coordinates = []
+for line in args.infile:
+  line = line.strip()
+  if line.startswith('#'):
+    continue
+  xStr, yStr, zStr = line.split()
+  x = float(xStr)
+  y = float(yStr)
+  z = float(zStr)
+  if x > args.xmin and x < args.xmax and y > args.ymin and y < args.ymax:
+    print(f'{xStr} {yStr} {zStr}', file=args.outfile)
+args.outfile.close()
