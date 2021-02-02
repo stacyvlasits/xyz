2c860da7a03851994a696d0a1a709f53149f562e
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2021-02-02 12:15:08 -0600

    Demo for Markus.
    Signed-off-by: Pablo Mayrgundter <pablo.mayrgundter@gmail.com>

--- index.js
+++ index.js
@@ -31,4 +31,17 @@ function handleHash() {
   let hash = location.hash.substr(1);
 }
 window.addEventListener('hashchange', handleHash);
+function onFileSelected() {
+//  console.log('uploaded files: ', this.files);
+  const file = this.files[0];
+  const reader = new FileReader();
+  reader.onload = content => {
+    const arr = content.target.result;
+    const base64 = arr.toString().split(',')[1];
+    const decoded = atob(base64);
+    console.log(decoded);
+  };
+  reader.readAsDataURL(file);
+}
+document.getElementById("uploadInput").addEventListener("change", onFileSelected, false);
 handleHash();
