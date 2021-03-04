77515b7be72fb116c826d12ecc93c56d7a98879d
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2021-03-03 23:26:01 -0600

    coords: added google map reference.

--- coords/index.js
+++ coords/index.js
@@ -1,3 +1,4 @@
+import * as Diurnal from '../lib/diurnal.js/diurnal.js';
 import {
   deg2rad,
   dms2deg,
@@ -5,9 +6,36 @@ import {
 } from './coords.js';
 
 
+Diurnal.bind();
+document.querySelector('#diurnal-controls').style.visibility = 'hidden';
+
+
 function $(id) {
   return document.getElementById(id);
 }
+
+let map, marker;
+
+
+// Initialize and add the map
+window.initMap = () => {
+  const bern = { lat: 46.951082773, lng: 7.438632421 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 17,
+    center: bern,
+  });
+  marker = new google.maps.Marker({
+    position: bern,
+    map: map,
+  });
+};
+
+
+function centerMap(lat, lon) {
+  marker.setPosition(new google.maps.LatLng(lat, lon));
+  map.panTo(new google.maps.LatLng(lat, lon));
+}
+
 
 
 function selectInput(value, degreesForm, dmsForm) {
@@ -23,9 +51,10 @@ function selectInput(value, degreesForm, dmsForm) {
 }
 
 
-function showOutput(N, E) {
+function showOutput(lat, lon, N, E) {
   const outputElt = document.querySelector('#output');
   outputElt.innerText = `${N} N ${E} E`;
+  centerMap(lat, lon);
 }
 
 
@@ -36,9 +65,9 @@ function init() {
   degreesForm.addEventListener('submit', event => {
     try {
       const lat = parseFloat(degreesForm.latDeg.value);
-      const lng = parseFloat(degreesForm.lngDeg.value);
-      const [N, E] = wgs2lv95(lat, lng);
-      showOutput(N, E);
+      const lon = parseFloat(degreesForm.lngDeg.value);
+      const [N, E] = wgs2lv95(lat, lon);
+      showOutput(lat, lon, N, E);
     } catch (e) {
       console.error(e);
     }
@@ -53,11 +82,11 @@ function init() {
       const lat = dms2deg(parseFloat(dmsForm.latDeg.value),
                           parseFloat(dmsForm.latMin.value),
                           parseFloat(dmsForm.latSec.value));
-      const lng = dms2deg(parseFloat(dmsForm.lngDeg.value),
+      const lon = dms2deg(parseFloat(dmsForm.lngDeg.value),
                           parseFloat(dmsForm.lngMin.value),
                           parseFloat(dmsForm.lngSec.value));
-      const [N, E] = wgs2lv95(lat, lng);
-      showOutput(N, E);
+      const [N, E] = wgs2lv95(lat, lon);
+      showOutput(lat, lon, N, E);
     } catch (e) {
       console.error(e);
     }
@@ -71,5 +100,6 @@ function init() {
 
   degreesForm.dispatchEvent(new Event('submit'));
 }
+
 
 init();
