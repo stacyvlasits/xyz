ad4df76c064fa4988f38100fe52f1d5f2bf13bdb
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2021-05-05 00:11:40 -0400

    project.xml download working

--- js/project/ProjectTools.vue
+++ js/project/ProjectTools.vue
@@ -1,11 +1,15 @@
 <template>
   Project Tools
-  <button v-on:click="onClick">Download</button>
-  Crd lat: {{coordinate.lat}}, lon: {{coordinate.lon}}, N: {{coordinate.N}}, E: {{coordinate.E}}<br/>
+  <button ref="dlBtn" href="" v-on:click="onClick">Download</button>
+<!--  Crd lat: {{coordinate.lat}}, lon: {{coordinate.lon}}, N: {{coordinate.N}}, E: {{coordinate.E}}<br/>
   Min lat: {{min.lat}}, lon: {{min.lon}}, N: {{min.N}}, E: {{min.E}}<br/>
-  Max lat: {{max.lat}}, lon: {{max.lon}}, N: {{max.N}}, E: {{max.E}}<br/>
+  Max lat: {{max.lat}}, lon: {{max.lon}}, N: {{max.N}}, E: {{max.E}}<br/> -->
 </template>
 <script>
+import {ref, onMounted} from 'vue';
+import {deg2dms} from '../coords/coords.js';
+
+let dlUrl = null;
 export default {
   props: {
     coordinate: Object,
@@ -15,9 +19,76 @@ export default {
   data() {
     return {}
   },
+  setup() {
+    const dlBtn = ref(null);
+    onMounted(() => {
+      // the DOM element will be assigned to the ref after initial render
+      console.log(dlBtn.value) // <div>This is a root element</div>
+    })
+    return {
+      dlBtn
+    }
+  },
+  watch: {
+    coordinate: {
+      handler() {
+        this.validateInput();
+      },
+      deep: true
+    },
+    min: {
+      handler() {
+        this.validateInput();
+      },
+      deep: true
+    },
+    max: {
+      handler() {
+        this.validateInput();
+      },
+      deep: true
+    },
+  },
   methods: {
+    validateInput() {
+      const lat = deg2dms(this.coordinate.lat);
+      const lon = deg2dms(this.coordinate.lon);
+      const fileContent = `<?xml version="1.0" encoding="UTF-8"?>
+<ProjectLocation>
+  <Version val="1"/>
+  <LatitudeCoord deg="${lat.deg}" min="${lat.min}" sec="${lat.sec}"/>
+  <LatitudeNorth val="${this.coordinate.N}"/>
+  <LongitudeCoord deg="${lon.deg}" min="${lon.min}" sec="${lon.sec}"/>
+  <LongitudeEast val="${this.coordinate.E}"/>
+  <LongLatUnit val="11"/>
+  <TimeZoneHour val="1"/>
+  <TimeZoneMinute val="0"/>
+  <AltitudeMeter val="0"/>
+  <AltitudeUnit val="1"/>
+  <NorthDirection val="0"/>"
+  <NorthAngleUnit val="0"/>
+</ProjectLocation>`;
+      const chk = Number.isFinite;
+      if (!(chk(lat.deg) && chk(lat.min) && chk(lat.sec)
+            && chk(lon.deg) && chk(lon.min) && chk(lon.sec))) {
+        dlUrl = null;
+        this.dlBtn.setAttribute('href', '');
+        console.log('Invalid coords: ', fileContent);
+        return;
+      }
+      const data = new Blob([fileContent], {type: 'text/xml;charset=utf-8'});
+      const url = window.URL.createObjectURL(data);
+      dlUrl = url;
+    },
     onClick(ev) {
       console.log('ProjectTools#onClick', ev);
+      const anchor = document.createElement('a');
+      anchor.href = dlUrl;
+      anchor.target = "_blank";
+      anchor.download = "project.xml";
+      anchor.click();
+      //this.dlBtn.setAttribute('href', dlUrl);
+      //window.URL.revokeObject(dlUrl);
     }
   }
 }
